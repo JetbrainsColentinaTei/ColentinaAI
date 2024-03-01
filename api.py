4d095b4b96585c0d535f0b24d09cc423bca9c5e0
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2024-03-01 20:18:36 +0100

    Separated logic

---
+++ api.py
@@ -0,0 +1,34 @@
+import os
+import socket
+from grazie.api.client.gateway import AuthType, GrazieApiGatewayClient, GrazieHeaders
+from grazie.api.client.chat.prompt import ChatPrompt
+from grazie.api.client.endpoints import GrazieApiGatewayUrls
+from grazie.api.client.profiles import Profile
+
+
+class GrazieApiGateWayClient:
+    def __init__(self):
+        self.token = os.getenv('GRAZIE_JWT_TOKEN')
+        self.ip = socket.gethostbyname(socket.gethostname())
+        if self.token is None:
+            raise ValueError("GRAZIE_JWT_TOKEN environment variable is not set. Please set it to your JWT token.")
+        print(self.token)
+
+    def chat(self):
+        client = GrazieApiGatewayClient(
+            url=GrazieApiGatewayUrls.PRODUCTION,
+            grazie_jwt_token=self.token,
+            auth_type=AuthType.APPLICATION,
+        )
+        response = client.chat(
+            chat=(
+                ChatPrompt()
+                .add_system("You are a helpful assistant.")
+                .add_user("Who won the world series in 2020?")
+            ),
+            profile=Profile.OPENAI_GPT_4,
+            headers={
+                GrazieHeaders.ORIGINAL_USER_IP: self.ip,
+            }
+        )
+        return response.content
--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-import os
-import socket
-from grazie.api.client.gateway import AuthType, GrazieApiGatewayClient, GrazieHeaders
-from grazie.api.client.chat.prompt import ChatPrompt
-from grazie.api.client.endpoints import GrazieApiGatewayUrls
-from grazie.api.client.profiles import Profile
+from api import GrazieApiGateWayClient
 
-token = os.getenv('GRAZIE_JWT_TOKEN')
+api = GrazieApiGateWayClient()
 
-if token is None:
-    raise ValueError("GRAZIE_JWT_TOKEN environment variable is not set. Please set it to your JWT token.")
-
-print(token)
-
-client_ip = socket.gethostbyname(socket.gethostname())
-client = GrazieApiGatewayClient(
-    url=GrazieApiGatewayUrls.PRODUCTION,
-    grazie_jwt_token=token,
-    auth_type=AuthType.APPLICATION,
-)
-response = client.chat(
-    chat=(
-        ChatPrompt()
-        .add_system("You are a helpful assistant.")
-        .add_user("Who won the world series in 2020?")
-    ),
-    profile=Profile.OPENAI_GPT_4,
-    headers={
-        GrazieHeaders.ORIGINAL_USER_IP: client_ip,
-    }
-)
-print(response.content)
+print(api.chat())
